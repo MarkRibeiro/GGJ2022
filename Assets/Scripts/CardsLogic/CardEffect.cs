34e9fa165aedfb3bdebe1211c1fe9de2b2d107dd
Nagib Moura Suaid <nagibmoura@gmail.com>
2022-01-29 11:18:03 -0300

    bugfix

--- Assets/Scripts/CardsLogic/CardEffect.cs
+++ Assets/Scripts/CardsLogic/CardEffect.cs
@@ -1,9 +1,10 @@
-
-
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 [System.Serializable]
 public class CardEffect
 {
-    int shieldDamage;
-    int lifeDamage;
-    bool ignoreShield;
+    public int shieldDamage;
+    public int lifeDamage;
+    public bool ignoreShield;
 }
