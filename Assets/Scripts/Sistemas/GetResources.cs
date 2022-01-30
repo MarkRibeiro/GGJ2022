38826eac4121e848a7969552352b90568647b8b3
AlOzorio <39777406+AlOzorio@users.noreply.github.com>
2022-01-29 22:47:23 -0300

    Correção texto dos recursos

--- Assets/Scripts/Sistemas/GetResources.cs
+++ Assets/Scripts/Sistemas/GetResources.cs
@@ -6,23 +6,24 @@ using TMPro;
 public class GetResources : MonoBehaviour
 {
     public DeckManager dm;
+    public BattleSystem bs;
     public TextMeshProUGUI p_EmotionText, p_ReasonText, e_EmotionText, e_ReasonText;
 
     // Start is called before the first frame update
     void Start()
     {
-        p_EmotionText.text = dm.player.emotion.ToString();
-        p_ReasonText.text = dm.player.reason.ToString();
-        e_EmotionText.text = dm.enemy.emotion.ToString();
-        e_ReasonText.text = dm.enemy.reason.ToString();
+        p_EmotionText.text = dm.player.emotion.ToString() + "/" + bs.resource_limit.ToString();
+        p_ReasonText.text = dm.player.reason.ToString() + "/" + bs.resource_limit.ToString();
+        e_EmotionText.text = dm.enemy.emotion.ToString() + "/" + bs.resource_limit.ToString();
+        e_ReasonText.text = dm.enemy.reason.ToString() + "/" + bs.resource_limit.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        p_EmotionText.text = dm.player.emotion.ToString();
-        p_ReasonText.text = dm.player.reason.ToString();
-        e_EmotionText.text = dm.enemy.emotion.ToString();
-        e_ReasonText.text = dm.enemy.reason.ToString();
+        p_EmotionText.text = dm.player.emotion.ToString() + "/" + bs.resource_limit.ToString();
+        p_ReasonText.text = dm.player.reason.ToString() + "/" + bs.resource_limit.ToString();
+        e_EmotionText.text = dm.enemy.emotion.ToString() + "/" + bs.resource_limit.ToString();
+        e_ReasonText.text = dm.enemy.reason.ToString() + "/" + bs.resource_limit.ToString();
     }
 }
