051fd900127e0e8bcf3b40cd1496bca4edbf7603
AlOzorio <39777406+AlOzorio@users.noreply.github.com>
2022-02-02 22:33:43 -0300

    Caixas de recursos das cores certas

--- Assets/Scripts/Sistemas/GetResources.cs
+++ Assets/Scripts/Sistemas/GetResources.cs
@@ -9,8 +9,12 @@ public class GetResources : MonoBehaviour
     public DeckManager dm;
     public BattleSystem bs;
     public Image p_EmotionImage, p_ReasonImage, e_EmotionImage, e_ReasonImage;
+    public Image p_EmotionBox, p_ReasonBox, e_EmotionBox, e_ReasonBox;
+
     public TextMeshProUGUI p_EmotionText, p_ReasonText, e_EmotionText, e_ReasonText;
     public Sprite p_EmotionSprite, p_ReasonSprite, y_EmotionSprite, y_ReasonSprite;
+    public Sprite p_BoxSprite, y_BoxSprite;
+
 
     // Start is called before the first frame update
     void Start()
@@ -24,17 +28,25 @@ public class GetResources : MonoBehaviour
         {
             p_EmotionImage.sprite = y_EmotionSprite;
             p_ReasonImage.sprite = y_ReasonSprite;
+            p_EmotionBox.sprite = y_BoxSprite;
+            p_ReasonBox.sprite = y_BoxSprite;
 
             e_EmotionImage.sprite = p_EmotionSprite;
             e_ReasonImage.sprite = p_ReasonSprite;
+            e_EmotionBox.sprite = p_BoxSprite;
+            e_ReasonBox.sprite = p_BoxSprite;
         }
         else
         {
             p_EmotionImage.sprite = p_EmotionSprite;
             p_ReasonImage.sprite = p_ReasonSprite;
+            p_EmotionBox.sprite = p_BoxSprite;
+            p_ReasonBox.sprite = p_BoxSprite;
 
             e_EmotionImage.sprite = y_EmotionSprite;
             e_ReasonImage.sprite = y_ReasonSprite;
+            e_EmotionBox.sprite = y_BoxSprite;
+            e_ReasonBox.sprite = y_BoxSprite;
         }
     }
 
