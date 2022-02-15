adc0f1158c16ec7904e1216f814d5ca0cfea6178
Nagib Moura Suaid <nagibmoura@gmail.com>
2022-02-14 23:17:31 -0300

    update dropdown value

--- Assets/Scripts/ChangeLocale.cs
+++ Assets/Scripts/ChangeLocale.cs
@@ -1,12 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Localization.Settings;
 
 public class ChangeLocale : MonoBehaviour
 {
+    public TMP_Dropdown dropdown;
     public void SetLocale(int i)
     {
         LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[i];
+    }
+
+    private void OnEnable()
+    {
+        if (dropdown == null)
+        {
+            dropdown = GetComponent<TMP_Dropdown>();
+        }
+        if (dropdown != null)
+        {
+            dropdown.value = LocalizationSettings.AvailableLocales.Locales.IndexOf(LocalizationSettings.SelectedLocale);
+        }
+        else
+        {
+            Debug.LogWarning("No dropdown found");
+        }
     }
 }
